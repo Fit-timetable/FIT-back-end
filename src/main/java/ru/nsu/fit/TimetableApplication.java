ec3066d19a842053e832e4aa29139f13d47e2820
MarinaSenoshenko <m.senoshenko@g.nsu.ru>
2023-10-19 11:15:46 +0700

    first commit

--- src/main/java/ru/nsu/fit/TimetableApplication.java
+++ src/main/java/ru/nsu/fit/TimetableApplication.java
@@ -2,11 +2,13 @@ package ru.nsu.fit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.nsu.fit.parser.WebParser;
 
 @SpringBootApplication
 public class TimetableApplication {
 
     public static void main(String[] args) {
+        new WebParser();
         SpringApplication.run(TimetableApplication.class, args);
     }
 
