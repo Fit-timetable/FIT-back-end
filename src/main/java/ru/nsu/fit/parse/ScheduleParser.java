f81dadae01564d75b01ca4d69596865fb15bdf8f
MarinaSenoshenko <m.senoshenko@g.nsu.ru>
2023-10-20 22:13:50 +0700

    fixes delete lombok and rename lesson which forgot to rename

--- src/main/java/ru/nsu/fit/parse/ScheduleParser.java
+++ src/main/java/ru/nsu/fit/parse/ScheduleParser.java
@@ -7,7 +7,7 @@ import org.jsoup.select.Elements;
 import java.util.*;
 
 public class ScheduleParser {
-    private final List<Lesson>[][] subjects = new ArrayList[DAYS_IN_WEEK][LESSONS_IN_DAY];
+    private final List<Lesson>[][] lessons = new ArrayList[DAYS_IN_WEEK][LESSONS_IN_DAY];
 
     private static final int DAYS_IN_WEEK = 6;
     private static final int LESSONS_IN_DAY = 7;
@@ -42,7 +42,7 @@ public class ScheduleParser {
                         if (!cells.isEmpty()) {
                             Element currentCell = cells.get(i + 1);
 
-                            subjects[i][j] = new ArrayList<>();
+                            lessons[i][j] = new ArrayList<>();
                             for (int k = 0; k < currentCell.select(".subject").size(); k++) {
                                 String subject = getElementText(currentCell.select(".subject"), k);
                                 String tutor = getElementText(currentCell.select(".tutor"), k);
@@ -50,7 +50,7 @@ public class ScheduleParser {
                                 String type = getElementText(currentCell.select(".type"), k);
                                 String week = getElementText(currentCell.select(".week"), k);
 
-                                subjects[i][j].add(new Lesson(subject, tutor, room, type, week));
+                                lessons[i][j].add(new Lesson(subject, tutor, room, type, week));
                             }
                             j++;
                         }
@@ -69,15 +69,16 @@ public class ScheduleParser {
         for (int i = 0; i < DAYS_IN_WEEK; i++) {
             for (int j = 0; j < LESSONS_IN_DAY; j++) {
                 System.out.print(j + 1 + ". ");
-                for (int k = 0; k < subjects[i][j].size(); k++) {
+                for (int k = 0; k < lessons[i][j].size(); k++) {
                     if (k > 0) {
                         System.out.print(" / ");
                     }
-                    System.out.print(subjects[i][j].get(k).subject() + " " +
-                                    subjects[i][j].get(k).type() + " " +
-                                    subjects[i][j].get(k).tutor() + " " +
-                                    subjects[i][j].get(k).room() + " " +
-                                    subjects[i][j].get(k).week());
+                    System.out.print(
+                            lessons[i][j].get(k).subject() + " " +
+                            lessons[i][j].get(k).type() + " " +
+                            lessons[i][j].get(k).tutor() + " " +
+                            lessons[i][j].get(k).room() + " " +
+                            lessons[i][j].get(k).week());
                 }
                 System.out.print("\n");
             }
