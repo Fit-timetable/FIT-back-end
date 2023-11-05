47d91befc98834877708382a946d6221d1f4a11c
MarinaSenoshenko <m.senoshenko@g.nsu.ru>
2023-11-05 18:54:32 +0700

    add get pinned schedule

--- src/main/java/ru/nsu/fit/schedule/api/ScheduleService.java
+++ src/main/java/ru/nsu/fit/schedule/api/ScheduleService.java
@@ -7,6 +7,8 @@ public interface ScheduleService {
 
     WeekScheduleDto getScheduleByRoom(String room);
 
+    WeekScheduleDto getPinnedSchedule(Long studentId);
+
     void pinSchedule(Long studentId, String group);
 
     void resetSchedule(Long studentId);
