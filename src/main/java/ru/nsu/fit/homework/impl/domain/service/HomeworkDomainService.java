64d5d205732911ac8f16fb94cee4b7f2c0a20400
Knyazhev-Misha <m.knyazhev@g.nsu.ru>
2023-12-04 06:17:33 +0700

    renames..

--- src/main/java/ru/nsu/fit/homework/impl/domain/service/HomeworkDomainService.java
+++ src/main/java/ru/nsu/fit/homework/impl/domain/service/HomeworkDomainService.java
@@ -7,25 +7,20 @@ import ru.nsu.fit.homework.api.dto.HomeworkResponseDto;
 import ru.nsu.fit.homework.impl.domain.model.Homework;
 import ru.nsu.fit.homework.impl.domain.model.HomeworkFile;
 
-import java.util.Optional;
-
 @Component
 @Getter
 @AllArgsConstructor
 public class HomeworkDomainService {
     public HomeworkResponseDto toHomeworkResponseDto(Homework homework, HomeworkFile homeworkFile, Boolean isShared){
-        String homeworkText = "";
-        Integer notificationPeriod = 0;
-
         return new HomeworkResponseDto(
                 homework.getId(),
                 homework.getStudentId(),
                 homework.getDeadline().toString(),
-                homeworkText,
+                homework.getHomeworkText(),
                 (int) homework.getEstimatedTime().getSeconds(),
                 homeworkFile.getFileDriveUri(),
                 isShared,
-                notificationPeriod,
+                (int)homework.getNotificationPeriod().getSeconds(),
                 homework.getDaysBeforeDeadlineReminder()
                 );
     }
