138848c646290f8ad5f2b0c5078f5e9712be00c2
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2023-12-06 06:17:10 +0700

    forgot file

--- src/main/java/ru/nsu/fit/group/impl/service/GroupServiceImpl.java
+++ src/main/java/ru/nsu/fit/group/impl/service/GroupServiceImpl.java
@@ -1,15 +1,11 @@
 package ru.nsu.fit.group.impl.service;
 
 import lombok.AllArgsConstructor;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import ru.nsu.fit.group.api.GroupDto;
 import ru.nsu.fit.group.api.GroupService;
-import ru.nsu.fit.group.impl.data.GroupRepository;
-import ru.nsu.fit.group.impl.data.specification.GroupSpecifications;
 import ru.nsu.fit.group.impl.data.GroupStudentRepository;
 import ru.nsu.fit.group.impl.domain.model.Group;
-import ru.nsu.fit.group.impl.domain.service.DomainGroupService;
 import ru.nsu.fit.group.impl.domain.service.GroupParser;
 
 import java.util.List;
@@ -19,8 +15,6 @@ import java.util.List;
 public class GroupServiceImpl implements GroupService {
 
     private final GroupStudentRepository groupStudentRepository;
-    private final GroupRepository groupRepository;
-    private final DomainGroupService domainGroupService;
 
     @Override
     public Group getGroupByStudentId(Long studentId) {
