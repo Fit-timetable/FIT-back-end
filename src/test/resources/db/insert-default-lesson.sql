cc8b0351d2007369a9d5114af5f12c9fc7e235a2
MarinaSenoshenko <m.senoshenko@g.nsu.ru>
2023-12-09 19:05:18 +0700

    first version lesson tests

---
+++ src/test/resources/db/insert-default-lesson.sql
@@ -0,0 +1,6 @@
+INSERT INTO subject ("name") VALUES ('Mathematics');
+
+INSERT INTO "group" (number) VALUES ('A1');
+
+INSERT INTO lesson (day_name, start_time, room, teacher, meet_link, "type", parity, subject_id, group_id)
+VALUES ('WEDNESDAY', '2022-01-12 14:30:00+00', 'Room 103', 'Robert Johnson', 'meetlink3', 'LECTURE', 'EVEN', 1, 1);
